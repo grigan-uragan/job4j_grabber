b186dcac6baf919d129421eee03054ddf102fb90
grigan-Uragan <prischepny@mail.ru>
2021-01-12 15:02:40 +0300

    2. Парсинг HTML страницы. [#355304]

---
+++ src/main/java/ru/grigan/grabber/SqlRuParse.java
@@ -0,0 +1,23 @@
+package ru.grigan.grabber;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class SqlRuParse {
+
+    public static void main(String[] args) throws IOException {
+        Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
+        Elements row = doc.select(".postslisttopic");
+        for (Element td : row) {
+            Element href = td.child(0);
+            Element other = td.parent().child(5);
+            System.out.println("1 " + href.attr("href"));
+            System.out.println("2 " + href.text());
+            System.out.println("3 " +  other.text());
+        }
+    }
+}
