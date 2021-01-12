a6dd9224950c261219cfe34302828ce0aa203637
grigan-Uragan <prischepny@mail.ru>
2021-01-12 22:39:21 +0300

    2.1.1. Парсинг https://www.sql.ru/forum/job-offers/3 [#355308]

--- src/main/java/ru/grigan/grabber/SqlRuParse.java
+++ src/main/java/ru/grigan/grabber/SqlRuParse.java
@@ -8,18 +8,38 @@ import org.jsoup.select.Elements;
 import java.io.IOException;
 
 public class SqlRuParse {
+private static int count = 0;
 
     public static void main(String[] args) throws IOException {
         Document doc = Jsoup.connect("https://www.sql.ru/forum/job-offers").get();
         Elements row = doc.select(".postslisttopic");
-        for (Element td : row) {
+        parsePage(row);
+        Elements href = doc.select(".sort_options");
+        for (Document document : getPage(doc, 5)) {
+            parsePage(document.select(".postslisttopic"));
+        }
+    }
+
+    // this method parsing page
+    public static void parsePage(Elements source) {
+        for (Element td : source) {
             Element href = td.child(0);
             Element other = td.parent().child(5);
-            System.out.println("1 " + href.attr("href"));
-            System.out.println("2 " + href.text());
-            System.out.println("3 " + new RussianDateFormat().
+            System.out.println(++count + ") " + href.attr("href"));
+            System.out.println(href.text());
+            System.out.println(new RussianDateFormat().
                     getDateFromRusFormat(other.text(),
-                    RussianDateFormat.PATTERN));
+                            RussianDateFormat.PATTERN));
         }
+    }
+
+    //this method parse link for new page
+    public static Document[] getPage(Document document, int pages) throws IOException {
+        Document[] doc = new Document[pages];
+        Elements links = document.select(".sort_options").select("a");
+        for (int i = 0; i < doc.length; i++) {
+            doc[i] = Jsoup.connect(links.get(i).attr("href")).get();
+        }
+        return doc;
     }
 }
