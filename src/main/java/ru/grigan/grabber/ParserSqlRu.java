46cae2fe9211737af3bfff70a0e4b2582dd85c7c
grigan-Uragan <prischepny@mail.ru>
2021-01-14 16:03:15 +0300

    2.4. SqlRuParse [#355311] refactor getPost method

--- src/main/java/ru/grigan/grabber/ParserSqlRu.java
+++ src/main/java/ru/grigan/grabber/ParserSqlRu.java
@@ -25,17 +25,9 @@ public class ParserSqlRu implements Parse {
         Elements row = doc.select(".postslisttopic");
         for (Element element : row) {
             Element elementForPostInstance = element.child(0);
-            Element elementForDate = element.parent().child(5);
-            Date date = new RussianDateFormat().getDateFromRusFormat(
-                    elementForDate.text(), RussianDateFormat.PATTERN);
-            String title = elementForPostInstance.text();
             String link = elementForPostInstance.attr("href");
-            Post post = getPost(link);
-            post.setTitle(title);
-            post.setDateCreation(date);
-            posts.add(post);
+            posts.add(getPost(link));
         }
-
         return posts;
     }
 
@@ -43,6 +35,16 @@ public class ParserSqlRu implements Parse {
     public Post getPost(String url) throws IOException {
         Document document = Jsoup.connect(url).get();
         Elements elements = document.select(".msgBody");
+        String title = document.select(".messageHeader").first().text();
+        if (title.contains("[new]")) {
+           title = title.replace("[new]", "").strip();
+        }
+        String footer = document.select(".msgFooter")
+                .first()
+                .text();
+        String stringDate = footer.substring(0, footer.indexOf("[")).strip();
+        Date date = new RussianDateFormat()
+                .getDateFromRusFormat(stringDate, RussianDateFormat.PATTERN);
         String body = elements.select(".msgBody").last().text();
         String description = elements.select(".msgBody").first().text();
         String username;
@@ -50,15 +52,11 @@ public class ParserSqlRu implements Parse {
             username = description.substring(0, description.indexOf("Member") - 1);
         } else if (description.contains("Администратор")) {
             username = description.substring(0, description.indexOf("Администратор") - 1);
-
         } else {
             username = "_______________";
         }
-        Post post = new Post();
-        post.setUrl(url);
-        post.setUsername(username);
-        post.setDescription(body);
-        return post;
+
+        return new Post(url, title, username, body, date);
     }
 
     public String[] getUrls() {
@@ -69,14 +67,9 @@ public class ParserSqlRu implements Parse {
         this.urls = urls;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ParserSqlRu parserSqlRu = new ParserSqlRu();
-        for (String url : parserSqlRu.getUrls()) {
-            try {
-                parserSqlRu.getPostList(url).forEach(System.out::println);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
+        parserSqlRu.getPostList("https://www.sql.ru/forum/job-offers/1")
+                .forEach(System.out::println);
     }
 }
