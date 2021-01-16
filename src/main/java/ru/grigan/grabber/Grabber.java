7f9eb2cb5a884b9a86f31784e4f0bcbc9f6e4490
grigan-Uragan <prischepny@mail.ru>
2021-01-17 00:08:52 +0300

    7. Web.

--- src/main/java/ru/grigan/grabber/Grabber.java
+++ src/main/java/ru/grigan/grabber/Grabber.java
@@ -5,6 +5,10 @@ import org.quartz.impl.StdSchedulerFactory;
 
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
 import java.util.Properties;
 
 public class Grabber implements Grab {
@@ -46,6 +50,30 @@ public class Grabber implements Grab {
         return StdSchedulerFactory.getDefaultScheduler();
     }
 
+    public void web(Store store) {
+        new Thread(() -> {
+           try (ServerSocket server =
+                        new ServerSocket(Integer.parseInt(properties.getProperty("port")))) {
+               while (!server.isClosed()) {
+                   Socket socket = server.accept();
+                   try (OutputStream outputStream = socket.getOutputStream()) {
+                       outputStream.write("HTTP/1.1 200 OK\r\n\r\n"
+                               .getBytes(StandardCharsets.UTF_8));
+                       for (Post post : store.getAll()) {
+                           outputStream.write(String.format("<p>%s</p>", post.toString())
+                                   .getBytes(StandardCharsets.UTF_8));
+                           outputStream.write(System.lineSeparator()
+                                   .getBytes(StandardCharsets.UTF_8));
+                       }
+                   }
+               }
+           } catch (IOException e) {
+               e.printStackTrace();
+               System.out.println("server falling");
+           }
+        }).start();
+    }
+
     public static void main(String[] args) throws SchedulerException {
         Properties properties = new Properties();
         try (FileInputStream inputStream = new FileInputStream(
@@ -60,6 +88,7 @@ public class Grabber implements Grab {
         Scheduler scheduler = grabber.getScheduler();
         grabber.init(parserSqlRu, pSqlStore, scheduler);
         pSqlStore.getAll().forEach(System.out::println);
+        grabber.web(pSqlStore);
     }
 
 }
